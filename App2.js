1eb280abf5cc2cde1ef90e8a1cc1e7c8b3d1853d
justinng01 <95277178+justinng01@users.noreply.github.com>
2022-02-04 11:03:35 +0800

    Update App2.js

--- App2.js
+++ App2.js
@@ -4,158 +4,148 @@
 
 var Contracts = { BookingContract: {
     abi: [
-        {
-            "inputs": [],
-            "name": "bookingCount",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "name": "bookingList",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "bookingNo",
-                    "type": "uint256"
-                },
-                {
-                    "internalType": "string",
-                    "name": "documentNo",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "receiver",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "locker",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "booker",
-                    "type": "string"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [],
-            "name": "receiveCount",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "name": "receiveList",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "receiveNo",
-                    "type": "uint256"
-                },
-                {
-                    "internalType": "string",
-                    "name": "receiver2",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "locker2",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "booker2",
-                    "type": "string"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "_documentNo",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "_receiver",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "_locker",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "_booker",
-                    "type": "string"
-                }
-            ],
-            "name": "registerNewBooking",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "_receiver2",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "_locker2",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "_booker2",
-                    "type": "string"
-                }
-            ],
-            "name": "registerNewReceive",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ],
-    address: "0x1266e694D2B237A337D79152a7D1904db62A5560",
+	{
+		"inputs": [],
+		"name": "bookingCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "bookingList",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "bookingNo",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "documentNo",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "receiver",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "locker",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "booker",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "receiveCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "receiveList",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "receiveNo",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "receiver2",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "locker2",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_documentNo",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_receiver",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_locker",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_booker",
+				"type": "string"
+			}
+		],
+		"name": "registerNewBooking",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_receiver2",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_locker2",
+				"type": "string"
+			}
+		],
+		"name": "registerNewReceive",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+],
+    address: "0x36bCd29A0ab172fd55A7E4015329D5916e39DdE7",
     endpoint: "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
     }}
 
